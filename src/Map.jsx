65bc42aebfddf467f40914027c637950fbd82c6a
Sofiya Kunitsyna <31761683+Kunica97@users.noreply.github.com>
2018-05-08 13:04:57 +0300

    Add files via upload

---
+++ src/Map.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Map = () =>
+	(<div><h1>Карта</h1><iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A4f2676598d5b7bc71c25915d07c8e969c8bf98102f8442eb3cc68073e361efe9&amp;source=constructor" width="500" height="400" frameborder="0"></iframe></div>);
+
+export default Map;
